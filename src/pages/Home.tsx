4350f5b68089c896c6a643abe314db15e097515f
son.tran <son.tran@sweetsoft.vn>
2025-05-27 15:21:58 +0700

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,37 @@
+import { useEffect } from 'react'
+
 const Home = () => {
+  useEffect(() => {
+    document.title = 'Trang chủ | Smarter'
+
+    // Hàm thêm hoặc cập nhật một thẻ meta
+    const updateMetaTag = (
+      attr: string,
+      attrValue: string,
+      content: string
+    ) => {
+      let element = document.querySelector(`meta[${attr}='${attrValue}']`)
+      if (!element) {
+        element = document.createElement('meta')
+        element.setAttribute(attr, attrValue)
+        document.head.appendChild(element)
+      }
+      element.setAttribute('content', content)
+    }
+
+    updateMetaTag(
+      'property',
+      'og:image',
+      'https://static.vecteezy.com/system/resources/thumbnails/014/440/983/small_2x/image-icon-design-in-blue-circle-png.png'
+    )
+    updateMetaTag('property', 'og:description', 'Mô tả nội dung động từ React')
+    updateMetaTag(
+      'name',
+      'description',
+      'Trang chủ Smarter, cập nhật meta động'
+    )
+  }, [])
+
   return <div>Home</div>
 }
 
